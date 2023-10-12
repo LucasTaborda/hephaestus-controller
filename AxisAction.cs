b2ca5ad9f20c7add57caf922ffb796180eab91ae
Lucas Taborda <36085949+LucasTaborda@users.noreply.github.com>
2023-10-11 22:14:14 -0300

    Update and rename InputAction.cs to AxisAction.cs

---
+++ AxisAction.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hephaestus
+{
+    public class AxisAction : MonoBehaviour, IInputAction
+    {
+        [SerializeField] protected string[] _axisesToGet;
+        protected Dictionary<string, float> _inputs = new();
+
+        void Awake()
+        {
+            foreach (var name in _axisesToGet)
+            {
+                _inputs.Add(name, 0);
+            }
+        }
+
+
+        protected virtual void Update()
+        {
+            GetInputs();
+        }
+
+
+        private void GetInputs()
+        {
+            foreach (var name in _axisesToGet)
+            {
+                _inputs[name] = Input.GetAxisRaw(name);
+            }
+        }
+    }
+}
