077320acea411e4e8b19dcacc030341fc6e5ba7a
Lucas Taborda <36085949+LucasTaborda@users.noreply.github.com>
2023-10-11 22:16:01 -0300

    Create IInputAction.cs

---
+++ IInputAction.cs
@@ -0,0 +1,15 @@
+namespace Hephaestus
+{
+    public interface IInputAction
+    {
+        protected virtual void Update()
+        {
+
+        }
+
+        private void GetInputs()
+        {
+
+        }
+    }
+}
